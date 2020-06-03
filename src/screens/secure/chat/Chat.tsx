4953c15cd9c9b085d2acf3e94e3af81d1ae1cb15
ragu <ragu25@codingtown.com>
2020-06-03 19:06:04 +0530

    Gifted chat

--- src/screens/secure/chat/Chat.tsx
+++ src/screens/secure/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Container,
   Header,
@@ -8,8 +8,30 @@ import {
   Left,
   Right,
 } from 'native-base';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 const ChatScreen: React.FC = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const message: any = {
+      _id: 1,
+      text: 'Hello developer',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'React Native',
+        avatar: 'https://placeimg.com/140/140/any',
+      },
+    };
+    setMessages([message]);
+  }, []);
+
+  const onSend = (message: any) => {
+    const updateMessge = GiftedChat.append(messages, message);
+    setMessages(updateMessge);
+  };
+
   return (
     <Container>
       <Header transparent={true}>
@@ -18,11 +40,17 @@ const ChatScreen: React.FC = () => {
         <Left />
         <Left />
         <Body>
-          <Title style={{color: '#000'}}>Chat</Title>
+          <Title style={{color: '#000'}}>Chats</Title>
         </Body>
         <Right />
       </Header>
-      <Content />
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     </Container>
   );
 };
